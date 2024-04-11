f53433faa7bbf89cb4221cf848a505f3dfbc2c35
Ermias Wakgari Yadeta <118937954+ERMIASWAKGARI@users.noreply.github.com>
2024-04-11 09:48:41 +0300

    ninth commit

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+//Environment variable
+// console.log(app.get('env'));
+console.log(process.env);
 
 const port = 3000;
 app.listen(port, () => {
